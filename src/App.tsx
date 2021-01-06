81d6106c64621d766b2c8c9fb2243e3c41dcff7a
Pavel Petrovich <gaara33377@gmail.com>
2021-01-06 17:08:28 +0300

    migrate to tsx compiler

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Aside from "./Components/Aside/Aside";
+import ChatContainer from "./Components/ChatContainer/ChatContainer";
+import classes from "./App.module.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => (
+  <div className={classes.container}>
+    <Aside />
+    <ChatContainer />
+  </div>
+);
 
 export default App;
---
+++ src/Components/UserBlock/UserBlock.tsx
@@ -0,0 +1,30 @@
+import ChevronDown from "../ChevronDown/ChevronDown";
+import classes from "./UserBlock.module.css";
+import maleAvatar from "./avatars/male_user.png";
+import { MouseEvent } from "react";
+
+const UserBlock = () => {
+  const handleClick = (e: MouseEvent) => {
+    e.stopPropagation();
+  };
+
+  return (
+    <div className={classes.userBlock}>
+      <div
+        style={{ backgroundImage: `url(${maleAvatar})` }}
+        className={classes.userAvatar}
+      />
+      <div
+        role="button"
+        aria-hidden={true}
+        onClick={handleClick}
+        className={classes.userInform}
+      >
+        <p className={classes.username}>Henry Jabbawockiez</p>
+        <ChevronDown />
+      </div>
+    </div>
+  );
+};
+
+export default UserBlock;
