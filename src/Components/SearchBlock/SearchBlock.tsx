75509285e0a6ab47d3af2bf1e2fe600348b4b1a5
Pavel Petrovich <gaara33377@gmail.com>
2021-01-08 14:50:10 +0300

    add app config

--- src/Components/SearchBlock/SearchBlock.tsx
+++ src/Components/SearchBlock/SearchBlock.tsx
@@ -1,15 +1,38 @@
-import { ChangeEvent, useState } from "react";
+import { ChangeEvent, useMemo, useState } from "react";
 import clsx from "clsx";
 import TextInput from "../common/TextInput/TextInput";
 import classes from "./SearchBlock.module.css";
 import Select from "../common/Select/Select";
 import SearchIcon from "../SearchIcon/SearchIcon";
+import config from "../../App.config.json";
 
 const SearchBlock = () => {
   const [searchValue, setSearchValue] = useState<string | number>("");
+  const [selectOption, setSelectOption] = useState<string>("");
 
   const handleSearchMessages = ({ target }: ChangeEvent<HTMLInputElement>) =>
     setSearchValue(target.value);
+
+  const handleSelectOptions = (event: ChangeEvent<HTMLInputElement>) => {
+    
+    if (typeof event === "string") {
+
+      setSelectOption(event);
+      return;
+    }
+
+    if (event.target.value === selectOption) {
+      return;
+    }
+
+    setSelectOption(event.target.value);
+  };
+
+  const options = useMemo(() => 
+  config.options.chatContainers.map(it => (
+    <option key={it.value} value={it.value}>{it.name}</option>
+  ))
+  , []);
 
   return (
     <div className={classes.searchBlock}>
@@ -23,7 +46,10 @@ const SearchBlock = () => {
       <Select
         placeholder="Messages"
         className={clsx(classes.searchController, classes.searchDropdown)}
+        onChange={handleSelectOptions}
+        value={selectOption}
         name="messages"
+        options={options}
       />
     </div>
   );
--- src/Components/common/Select/Select.tsx
+++ src/Components/common/Select/Select.tsx
@@ -1,4 +1,5 @@
 import clsx from "clsx";
+import { ChangeEventHandler, useEffect, useRef } from "react";
 import classes from "./Select.module.css";
 
 type SelectType = {
@@ -6,18 +7,42 @@ type SelectType = {
   placeholder?: string;
   name: string;
   disabled?: boolean;
+  onChange?: ChangeEventHandler;
+  value: string;
+  options: JSX.Element[];
+  defaultValue?: string;
 };
 
-const Select = ({ className, name, placeholder, disabled }: SelectType) => (
-  <select
-    className={clsx(classes.select, className)}
-    name={name}
-    placeholder={placeholder}
-    disabled={disabled}
-  >
-    <option value="messages">Messages</option>
-  </select>
-);
+const Select = ({ className, name, placeholder, disabled, value, onChange, options }: SelectType) => {
+
+  const selectRef = useRef<null | HTMLSelectElement>(null);
+
+  useEffect(() => {
+
+    if (value || !onChange || !selectRef.current) {
+      return;
+    }
+    
+    const { current } = selectRef;
+
+    onChange(current.value as any);
+
+  }, [selectRef, onChange, value]);
+  
+  return (
+    <select
+      ref={selectRef}
+      disabled={disabled || !options || !options?.length}
+      className={clsx(classes.select, className)}
+      name={name}
+      placeholder={placeholder}
+      onChange={onChange}
+      value={value}
+    >
+      {options}
+    </select>
+  );
+}
 
 Select.defaultProps = {
   className: "",
