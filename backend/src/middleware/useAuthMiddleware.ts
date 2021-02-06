9098ff611e479c8b817ad6a48682ae11c7d5ac86
Pavel Petrovich <gaara33377@gmail.com>
2021-02-06 04:21:01 +0300

    add auth starter

---
+++ backend/src/middleware/useAuthMiddleware.ts
@@ -0,0 +1,22 @@
+import passport from "passport";
+import db from "../database/models";
+import { ExpressUser } from "../interfaces";
+import { STRATEGY_CONTEXT } from "../models/AuthStrategyContext/AuthStrategyContext.constant";
+import AuthStrategyContext from "../models/AuthStrategyContext/AuthStrategyContext.model";
+
+export default () => {
+  passport.serializeUser((user, done) => done(null, (<ExpressUser>user).id));
+
+  passport.deserializeUser(async (id, done) => {
+    const currentUser = await db.User.findByPk(id as string);
+
+    if (currentUser === null) {
+      done(new Error("invalid user"));
+      return;
+    }
+
+    done(null, currentUser);
+  });
+
+  new AuthStrategyContext(STRATEGY_CONTEXT.DEFAULT).apply();
+};
